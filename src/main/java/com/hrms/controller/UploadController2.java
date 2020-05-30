cc9d6db7371523670c09bcf1f24e1b2375be77e7
MengFanWei <931388462@qq.com>
2020-05-30 23:12:36 +0800

    首次提交

---
+++ src/main/java/com/hrms/controller/UploadController2.java
@@ -0,0 +1,17 @@
+package com.hrms.controller;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+//类上面的【@RequestMapping】注解可以不要
+@RequestMapping(value = "/XXXX")
+public class UploadController2 {
+	
+	
+	
+}
---
+++ src/main/java/com/hrms/controller/UserController.java
@@ -0,0 +1,46 @@
+package com.hrms.controller;
+
+import java.io.UnsupportedEncodingException;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import org.junit.runner.Request;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.view.RedirectView;
+
+
+/**
+ * 注意：如果@Controller不指定其value【@Controller】，
+ * 则默认的bean名字为这个类的类名首字母小写，如果指定value【@Controller(value=”UserAction”)】
+ * 或者【@Controller(“UserAction”)】，
+ * 则使用value作为bean的名字。
+ * @author smfx1314
+ *
+ */
+@Controller
+@Scope("prototype")
+@RequestMapping("/upload")
+public class UserController{
+    /**
+     * 上传头像
+     * @param upload
+     * @return
+     */
+     @RequestMapping("/avatar")
+     @ResponseBody
+      public String uploadAvatar(MultipartFile upload){
+          //fileService.upLoadFile(upload);
+          return "ok";
+      }
+    
+ 
+}
