de4426a6cfd555093552c1aac349f841852d2127
MengFanWei <931388462@qq.com>
2020-06-07 03:29:48 +0800

    大量修改3

--- src/main/java/com/hrms/controller/EnvironmentalMonitoringController.java
+++ src/main/java/com/hrms/controller/EnvironmentalMonitoringController.java
@@ -1,23 +1,15 @@
 package com.hrms.controller;
 
-import com.hrms.bean.TblEmp;
 import com.hrms.bean.TblEnvironmentalMonitoring;
 import com.hrms.service.EnvironmentalMonitoringImpl;
-import com.hrms.service.UserServiceImpl;
-import com.hrms.util.JsonMsg;
-import org.apache.ibatis.annotations.Param;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpSession;
 import java.text.SimpleDateFormat;
-import java.util.Arrays;
 import java.util.List;
 
 /**
@@ -34,6 +26,7 @@ public class EnvironmentalMonitoringController {
     /**
      * 查询监测数据
      * @param deviceName
+     * @param monitoringTimeStr
      * @return
      */
     @RequestMapping(value = "/getEnvironmentalMonitoring", method = RequestMethod.GET)
